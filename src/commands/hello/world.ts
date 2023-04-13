bd8112842721334c7c37ec56d82adc482cc7545a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2023-04-13 15:28:15 +0900

    Impl --foo

--- src/commands/hello/world.ts
+++ src/commands/hello/world.ts
@@ -1,19 +1,30 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
 
 export default class World extends Command {
-  static description = 'Say hello world'
+  static description = 'Say hello world';
 
   static examples = [
     `<%= config.bin %> <%= command.id %>
 hello world! (./src/commands/hello/world.ts)
 `,
-  ]
+  ];
 
-  static flags = {}
+  static flags = {
+    foo: Flags.boolean({}),
+  };
 
-  static args = {}
+  static args = {};
 
   async run(): Promise<void> {
-    this.log('hello world! (./src/commands/hello/world.ts)')
+    const {flags} = await this.parse(World)
+    console.log('configDir', this.config.configDir)
+    if (flags.foo) {
+      this.log('--foo is given')
+      try {
+        World.run([])
+      } catch {}
+    } else {
+      this.log('hello world! (./src/commands/hello/world.ts)')
+    }
   }
 }
